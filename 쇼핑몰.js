0afaf92870985dfc9d3e892b43baf21e2a761d9f
co7744 <42232598+co7744@users.noreply.github.com>
2018-08-09 16:10:34 +0900

    first version for internet shopping (korean)

---
+++ 쇼핑몰.js
@@ -0,0 +1,13 @@
+/*쇼핑몰 javascript입니다.*/
+
+
+function query() {                    /*query()라는 서버점검중이라는 문구가 뜨게 되는 함수*/
+  var ret = confirm ("죄송합니다 서버점검중입니다. ");   /*확인을 눌러야 창이 꺼짐*/
+  return ret; 
+}
+
+function warning(){                   /*warning()라는 이라는 문구가 뜨게 되는 함수*/
+	var ret1=confirm("로그인을 먼저 해주세요! ");
+	return ret1;
+}
+
